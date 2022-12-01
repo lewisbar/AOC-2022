8b6f843e59a21270411bc15b1d6930611405512d
Lennart Wisbar <lewisbar.git@gmail.com>
2022-12-01 10:22:30 +0100

    Initial Commit

---
+++ AOC2022/AOC2022App.swift
@@ -0,0 +1,17 @@
+//
+//  AOC2022App.swift
+//  AOC2022
+//
+//  Created by LennartWisbar on 01.12.22.
+//
+
+import SwiftUI
+
+@main
+struct AOC2022App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AOC2022/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  AOC2022
+//
+//  Created by LennartWisbar on 01.12.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
