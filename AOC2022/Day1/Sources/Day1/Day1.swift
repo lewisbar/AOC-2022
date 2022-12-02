7bd394029a318368e61e316716a17ede7ecb13e4
Lennart Wisbar <lewisbar.git@gmail.com>
2022-12-02 09:17:35 +0100

    remove print statements from Day1

--- AOC2022/Day1/Sources/Day1/Day1.swift
+++ AOC2022/Day1/Sources/Day1/Day1.swift
@@ -17,15 +17,11 @@ public class Day1 {
     }
 
     public var solution1: String {
-        let result = String(elfWithMostCalories.totalCalories)
-        print("Day 1-1:", result)
-        return result
+        String(elfWithMostCalories.totalCalories)
     }
 
     public var solution2: String {
         let calories = topThreeElves.map { $0.totalCalories }
-        let result = String(calories.reduce(0, +))
-        print("Day 1-2", result)
-        return result
+        return String(calories.reduce(0, +))
     }
 }
