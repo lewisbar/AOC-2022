70c53859a41c9eb3c0a98cce4bb10b5da6f2c3fd
Lennart Wisbar <lewisbar.git@gmail.com>
2022-12-08 23:54:05 +0100

    fix parser tests

--- AOC2022/Day8/Tests/Day8Tests/ParserTests.swift
+++ AOC2022/Day8/Tests/Day8Tests/ParserTests.swift
@@ -8,57 +8,64 @@
 import XCTest
 @testable import Day8
 
-//final class ParserTests: XCTestCase {
-//
-//    // TODO: Does not work right now because of different IDs
-//    func test_parse_sampleInput_returnsForest() {
-//        let input = """
-//            30373
-//            25512
-//            65332
-//            33549
-//            35390
-//            """
-//        let parser = Parser()
-//        let expectedForest = Forest(rows: [
-//            Row(trees: [
-//                Tree(height: 3),
-//                Tree(height: 0),
-//                Tree(height: 3),
-//                Tree(height: 7),
-//                Tree(height: 3)
-//            ]),
-//            Row(trees: [
-//                Tree(height: 2),
-//                Tree(height: 5),
-//                Tree(height: 5),
-//                Tree(height: 1),
-//                Tree(height: 2)
-//            ]),
-//            Row(trees: [
-//                Tree(height: 6),
-//                Tree(height: 5),
-//                Tree(height: 3),
-//                Tree(height: 3),
-//                Tree(height: 2)
-//            ]),
-//            Row(trees: [
-//                Tree(height: 3),
-//                Tree(height: 3),
-//                Tree(height: 5),
-//                Tree(height: 4),
-//                Tree(height: 9)
-//            ]),
-//            Row(trees: [
-//                Tree(height: 3),
-//                Tree(height: 5),
-//                Tree(height: 3),
-//                Tree(height: 9),
-//                Tree(height: 0)
-//            ])
-//        ])
-//        let resultForest = parser.parse(input)
-//
-//        XCTAssertEqual(resultForest, expectedForest)
-//    }
-//}
+final class ParserTests: XCTestCase {
+    func test_parse_sampleInput_returnsForest() {
+        let input = """
+            30373
+            25512
+            65332
+            33549
+            35390
+            """
+        let expectedForest = Forest(rows: [
+            Row(trees: [
+                Tree(height: 3),
+                Tree(height: 0),
+                Tree(height: 3),
+                Tree(height: 7),
+                Tree(height: 3)
+            ]),
+            Row(trees: [
+                Tree(height: 2),
+                Tree(height: 5),
+                Tree(height: 5),
+                Tree(height: 1),
+                Tree(height: 2)
+            ]),
+            Row(trees: [
+                Tree(height: 6),
+                Tree(height: 5),
+                Tree(height: 3),
+                Tree(height: 3),
+                Tree(height: 2)
+            ]),
+            Row(trees: [
+                Tree(height: 3),
+                Tree(height: 3),
+                Tree(height: 5),
+                Tree(height: 4),
+                Tree(height: 9)
+            ]),
+            Row(trees: [
+                Tree(height: 3),
+                Tree(height: 5),
+                Tree(height: 3),
+                Tree(height: 9),
+                Tree(height: 0)
+            ])
+        ])
+        let parser = Parser()
+        let resultForest = parser.parse(input)
+
+        let expectedHeights = extractHeights(from: expectedForest)
+        let resultHeights = extractHeights(from: resultForest)
+
+        XCTAssertEqual(resultHeights, expectedHeights)
+    }
+
+    private func extractHeights(from forest: Forest) -> [[Int]] {
+        forest.rows.map { row in
+            row.trees.map { $0.height }
+        }
+    }
+}
